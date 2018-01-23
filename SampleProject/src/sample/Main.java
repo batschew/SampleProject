da39b0890c3a4253bda199ab4d9c63c9dad2f528
batschew <batschew@IL-PC-148.uccc.uc.edu>
2018-01-23 18:43:57 -0500

    First commit of basic "hello world" program

---
+++ SampleProject/src/sample/Main.java
@@ -0,0 +1,16 @@
+package sample;
+
+/**
+ * Sample project
+ * @author batschew
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("Hello world.");
+
+	}
+
+}
